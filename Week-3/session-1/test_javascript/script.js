c5dab84b7666c4c6c354c3b7f92f7375d3898dce
Jxshua-o <JoshuaDOlaleye@outlook.com>
2026-08-17 14:12:01 +0000

    wrking on javascript

---
+++ Week-3/session-1/test_javascript/script.js
@@ -0,0 +1,6 @@
+const title = document.getElementById('title');
+const button = document.getElementById('click');
+
+button.addEventListener('click',  function (){
+    title.textContent = 'Welcome to My Website';
+});
