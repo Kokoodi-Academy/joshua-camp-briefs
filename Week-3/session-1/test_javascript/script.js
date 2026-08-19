dc9afaa6d09287f7bb89bc531b7786098432f476
Jxshua-o <JoshuaDOlaleye@outlook.com>
2026-08-19 13:46:50 +0000

    made pizza website

--- Week-3/session-1/test_javascript/script.js
+++
@@ -1,6 +0,0 @@
-const title = document.getElementById("title");
-const button = document.getElementById("click");
-
-button.addEventListener("click",  function (){
-    title.textContent = "Welcome to My Website";
-});
