2390300789f6c4b9a164e60f7b2a60fa719777e2
Jxshua-o <JoshuaDOlaleye@outlook.com>
2026-08-20 21:02:58 +0000

    laptop hw

---
+++ Homework Folder/laptop/javascript.js
@@ -0,0 +1,7 @@
+document.querySelector("#shopButton").onclick = function () {
+    alert("Welcome to the NOVA laptop store!");
+};
+
+document.querySelector("#learnButton").onclick = function () {
+    document.querySelector("#laptops").scrollIntoView();
+};  
